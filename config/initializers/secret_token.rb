c640bc9fde627599c6e076d2cd3f10df256cfd57
Dhibika <dhibika.baskaran@sedin.co.in>
2013-03-04 20:18:50 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TruspuramLibrary::Application.config.secret_token = 'b110f14eb206908c74a8fe507cafc52b3812755fae36aa746cd7d4c967510feb6eeb862e563cdeb0c544b819cdf859e77ed4bb10a0b97e5f5163e1219edd6d36'
